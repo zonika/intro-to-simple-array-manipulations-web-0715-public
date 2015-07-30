057838dca13826f4da9720b26c17a90a690b73ec
zonika <zchodosh@conncoll.edu>
2015-07-30 15:30:49 -0400

    done

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,36 @@
+def using_push(array, val)
+  array.push(val)
+end
+def using_unshift(array, val)
+  array.unshift(val)
+end
+def using_pop(array)
+  array.pop
+end
+def pop_with_args(array)
+  array.pop(2)
+end
+def using_shift(array)
+  array.shift
+end
+def shift_with_args(array)
+  array.shift(2)
+end
+def using_concat(arr1,arr2)
+  arr1.concat(arr2)
+end
+def using_insert(array,val)
+  array.insert(4,val)
+end
+def using_uniq(array)
+  array.uniq
+end
+def using_flatten(array)
+  array.flatten
+end
+def using_delete(array,val)
+  array.delete(val)
+end
+def using_delete_at(array,val)
+  array.delete_at(val)
+end
